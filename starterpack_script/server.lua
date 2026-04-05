efdb56517958aa15712b13e3dea21899be53fd57
yoyo777-ops <dharshansm226@gmail.com>
2026-04-05 17:38:47 +0530

    removed unwanted lines of code

--- starterpack_script/server.lua
+++ starterpack_script/server.lua
@@ -26,26 +26,4 @@ lib.callback.register('ClaimStarterPack', function()
     return true, 'claimed'
 end)
 
---[[ RegisterNetEvent('ClaimStarterPack', function()
-    local src = source
-    local player = Ox.GetPlayer(src)
-    if not player then return end
 
-
-    local playerStateid = player.stateId
-    local checkclaimed = MySQL.scalar.await('SELECT stateid FROM starter_claims WHERE stateid = ?', { playerStateid })
-
-
-    if checkclaimed then
-        TriggerClientEvent('ox_lib:notify', src,
-            { type = 'error', description = 'you have already claimed the starter pack' })
-        return
-    end
-
-    MySQL.insert('INSERT INTO starter_claims (stateid) VALUES (?)', { playerStateid })
-
-    for key, value in pairs(items) do
-        exports.ox_inventory:AddItem(src, key, value)
-    end
-end)
- ]]
